46f1666a203d945f3563c8cf45af8ff465ba1103
28rahulmukherjee94 <88506235+28rahulmukherjee94@users.noreply.github.com>
2024-05-28 08:16:09 +0000

    Update main.tf 2nd commit

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  subscription_id            = 
-  client_id                  = 
-  client_secret              = 
-  tenant_id                  =  
+  # subscription_id            = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
+  # client_id                  = "b8b69685-6371-4547-90c9-f43bbacdfbb4"
+  # client_secret              = ""
+  # tenant_id                  = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
   features {
 
   }
@@ -35,6 +35,7 @@ resource "azurerm_virtual_network" "vnet1" {
 variable "storage_account_name" {
   type = string
   description = "Please enter teh storage account name"
+  default = "storage28novaccount"
   
 }
 
@@ -64,4 +65,20 @@ resource "azurerm_storage_blob" "iac" {
   type                   = "Block"
   source                 = "C:/Users/rahul.g.mukherjee/Documents/IAC/main.tf"
   depends_on = [ azurerm_storage_container.data ]
+}
+
+
+
+
+resource "azurerm_virtual_network" "app-vnet" {
+  name                = "app-vnet"
+  location            = "westus"
+  resource_group_name = local.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
 }
